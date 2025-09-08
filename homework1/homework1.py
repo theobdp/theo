07bed3475fd7274a19cbb8b804f7f48eca9cd9ba
Theo <theobarboudesplaces@berkeley.edu>
2025-09-08 16:38:52 -0700

    testing commitment

---
+++ homework1/homework1.py
@@ -0,0 +1,68 @@
+#File: homework1.py
+
+# --- Variable and Data Types ---
+
+a = 10
+print(a)
+print(type(a))
+#int, which is basically just an integer value
+
+b = 1.5
+print(b)
+print(type(b))
+#float, which is a number with a decimal point
+
+c = 3j
+print(c)
+print(type(c))
+#complex, which is a complex number where the j represents the imaginary component
+
+d = "hello"
+print(d)
+print(type(d))
+#string, which is a word or text
+
+e = [1,2,3]
+print(e)
+print(type(e))
+#list, collection of items that you can move around
+
+f = {"name": "Ellen", "favorite fruit": "strawberry"}
+print(f)
+print(type(f))
+#dict, collection of pairs that are like keys for each other
+
+g = (1,2)
+print(g)
+print(type(g))
+#like a list, but you can't move the items around
+
+h = ["apple", "banana", "strawberry"]
+print(h)
+print(type(h))
+#list, but with different data types as the items
+
+i = True
+print(i)
+print(type(i))
+#bool, which is a boolean, either true or false
+
+j = None
+print(j)
+print(type(j))
+#NoneType, which is basically an N/A
+
+k = [True, "blue", 12]
+print(k)
+print(type(k))
+#list, but you can use different data types in one list
+
+l = str(14)
+print(l)
+print(type(l))
+#string, the string version of 14. basically l is "14"
+
+m = 1e4
+print(m)
+print(type(m))
+#float, this is scientific notation and it seems to default to being a float
