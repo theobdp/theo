a907b254aa2e376d3b9e9ef0b82a9f93b66f5359
Theo <theobarboudesplaces@berkeley.edu>
2025-09-10 16:45:01 -0700

    saving hw2 first time

---
+++ homework2/homework2.py
@@ -0,0 +1,48 @@
+# File: homework2.py
+
+# Your file path should look like:
+# python_decal_fa25/yourname/homework2/homework2.py
+
+# Questions (Answer these in the homework2.py file as comments):
+
+# 1) What’s the difference between Git, GitHub, and Git Bash?
+
+# 2) What’s the difference between the terminal and the command line?
+
+# 3) How does Windows PowerShell differ from Git Bash?
+
+# 4) What’s the difference between Anaconda, conda, and Python?
+
+# 5) What is VS Code? 
+
+# 6) What is a Jupyter Notebook? How is it different from Jupyter Lab?
+
+# 7) What does ~/ mean?
+
+# 8) What’s the difference between an absolute path and a relative path?
+
+# 9) Imagine you're in your "yourname" repo. Write the absolute and relative paths to "course_assignments/homework2".
+
+# 10) What command lets you move from "course_assignments/homework2/" to "course_assignments/"?
+
+# 11) What would rm ./ do in your current directory? (Don’t try it!)
+
+# 12) What do the following commands do?
+# git add
+# git commit
+# git push
+
+# 13) What's the difference between "git add ." and "git add <file>"?
+
+# 14) What do "git status" and "git log -1" do?
+
+# 15) What’s the difference between cloning a repository and pulling from it?
+
+# 16) What has been your most frustrating bug or error in this class so far? How did you troubleshoot or fix it?
+
+# 17) What’s a question you still have? What’s something you’re confused about?
+
+# 18) Tell me a fun fact!
+
+# 19) Print your favorite math expression you've learned in Python so far. 
+# (Hint: Use print() and add a comment explaining what it does.)
